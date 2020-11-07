6a465935d10fcfdf67872c4e5f019ac051b730a1
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2020-11-08 06:11:33 +0800

    Run terraform fmt -recursive

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ module "google_postgres_db" {
 }
 
 resource "google_project_iam_member" "cloudsql_proxy_user" {
-  for_each      = toset(var.sql_proxy_user_groups)
+  for_each   = toset(var.sql_proxy_user_groups)
   role       = "roles/cloudsql.client" # see https://cloud.google.com/sql/docs/postgres/quickstart-proxy-test#before-you-begin
   member     = "group:${each.value}"
   depends_on = [google_project_service.compute_api, google_project_service.cloudsql_api]
