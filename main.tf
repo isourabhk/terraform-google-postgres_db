9eeb9977e97b2c279df2223b5cf00d9e169fb7b4
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2020-12-19 05:35:56 +0800

    Run terraform fmt -recursive

--- main.tf
+++ main.tf
@@ -35,29 +35,29 @@ resource "google_project_service" "cloudsql_api" {
 }
 
 module "google_postgres_db" {
-  source              = "GoogleCloudPlatform/sql-db/google//modules/postgresql"
-  version             = "4.3.0"
-  depends_on          = [google_project_service.compute_api, google_project_service.cloudsql_api]
-  deletion_protection = var.deletion_protection_master_instance
-  project_id          = data.google_client_config.google_client.project
-  name                = format("postgres-%s", local.master_instance_name_suffix)
+  source               = "GoogleCloudPlatform/sql-db/google//modules/postgresql"
+  version              = "4.3.0"
+  depends_on           = [google_project_service.compute_api, google_project_service.cloudsql_api]
+  deletion_protection  = var.deletion_protection_master_instance
+  project_id           = data.google_client_config.google_client.project
+  name                 = format("postgres-%s", local.master_instance_name_suffix)
   db_name              = var.default_db_name
   db_collation         = var.default_db_collation
   db_charset           = var.default_db_charset
-  database_version    = var.db_version
-  region              = data.google_client_config.google_client.region
-  zone                = format("%s-%s", data.google_client_config.google_client.region, var.zone_master_instance)
-  availability_type   = var.highly_available ? "REGIONAL" : "ZONAL"
-  tier                = var.instance_size_master_instance
-  disk_size           = var.disk_size_gb_master_instance
-  disk_autoresize     = var.disk_auto_resize_master_instance
-  disk_type           = "PD_SSD"
-  create_timeout      = var.db_timeout
-  update_timeout      = var.db_timeout
-  delete_timeout      = var.db_timeout
+  database_version     = var.db_version
+  region               = data.google_client_config.google_client.region
+  zone                 = format("%s-%s", data.google_client_config.google_client.region, var.zone_master_instance)
+  availability_type    = var.highly_available ? "REGIONAL" : "ZONAL"
+  tier                 = var.instance_size_master_instance
+  disk_size            = var.disk_size_gb_master_instance
+  disk_autoresize      = var.disk_auto_resize_master_instance
+  disk_type            = "PD_SSD"
+  create_timeout       = var.db_timeout
+  update_timeout       = var.db_timeout
+  delete_timeout       = var.db_timeout
   user_name            = var.root_user_name
   user_password        = var.root_user_password
-  database_flags      = local.db_flags_master_instance
+  database_flags       = local.db_flags_master_instance
   user_labels          = var.labels_master_instance
   additional_users     = var.additional_users
   additional_databases = var.additional_databases
